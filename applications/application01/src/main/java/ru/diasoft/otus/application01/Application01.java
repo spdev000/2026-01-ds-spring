b3c4310c11acb8275dfecf2bd9e0f7d66eb7e579
spdev000 <93079087+spdev000@users.noreply.github.com>
2026-02-18 17:07:08 +0300

    Домашнее задание 4. Переход на Spring Boot 3

--- applications/application01/src/main/java/ru/diasoft/otus/application01/Application01.java
+++ applications/application01/src/main/java/ru/diasoft/otus/application01/Application01.java
@@ -1,21 +1,21 @@
 package ru.diasoft.otus.application01;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import ru.diasoft.otus.application01.service.QuestionService;
 
-@PropertySource("classpath:application.properties")
-@ComponentScan
+@SpringBootApplication
 public class Application01 {
 
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application01.class)) {
+        SpringApplication.run(Application01.class, args);
+    }
 
-            QuestionService service = context.getBean(QuestionService.class);
-
-            service.procQuestion();
-        }
+    @Bean
+    public CommandLineRunner run(QuestionService service) {
+        return args -> service.procQuestion();
     }
 
 }
